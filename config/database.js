c6702aecf3a95a99772c4450c5fa55e9325f03f6
Muhammad Aadil <adilbanaras@gmail.com>
2021-02-12 18:15:21 +0400

    local db made postgres

--- config/database.js
+++ config/database.js
@@ -4,12 +4,41 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'changeme'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        },
       },
       options: {
-        useNullAsDefault: true,
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
 });
+
+
+
+// module.exports = {
+//   port: process.env.PORT || 3000,
+//   postgres: {
+//     HOST: 'localhost',
+//     USER: 'postgres',
+//     PASSWORD: 'password',
+//     DB: 'bbt',
+//     dialect: 'postgres',
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000,
+//     },
+//   },
+//   jwtKey: process.env.JWT_KEY || 'hahtesgsdfsdkljlhwjsdfsdf'
+
+// };
